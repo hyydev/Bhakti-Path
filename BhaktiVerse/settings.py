60ea54d5bb4316ca381f0fca45f0dfee1dce84c2
Devesh sharma <devsharma13314@gmail.com>
2025-05-20 01:17:10 +0530

    .env and gitignore db connection

--- BhaktiVerse/settings.py
+++ BhaktiVerse/settings.py
@@ -73,10 +73,20 @@ WSGI_APPLICATION = 'BhaktiVerse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
