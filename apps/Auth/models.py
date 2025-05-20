9aecab14fef6b93a8c045b83b563ff9e1c16fe0f
Devesh sharma <devsharma13314@gmail.com>
2025-05-20 18:22:46 +0530

    otpmodel creations

--- apps/Auth/models.py
+++ apps/Auth/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from apps.User.models import Baseclass,User
 
-# Create your models here.
+
+class OTPVerification(Baseclass):
+    """
+    OTP Verification model for user authentication.
+    """
+    id =models.UUIDField(primary_key=True, auto_created=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='otp_verification')
+    email = models.EmailField()
+    mobile_number = models.CharField(max_length=15)
+    otp_code = models.CharField(max_length=6)
+    is_verified = models.BooleanField(default=False)
+    expired_at = models.DateTimeField()
+    attempt_count = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return f"OTP for {self.user.full_name}"
+    
+    class Meta:
+        db_table = 'otp_verification'
+        ordering = ['-created_at']
+
+
