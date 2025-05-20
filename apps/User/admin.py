906808f8f816f7c568f8db5168350a96df332683
Devesh sharma <devsharma13314@gmail.com>
2025-05-20 17:07:25 +0530

    registermodels in admin panel

--- apps/User/admin.py
+++ apps/User/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, UserProfile, UserAddress
+
+admin.site.register(User)
+
+admin.site.register(UserProfile)
+
+admin.site.register(UserAddress)
+
+
 
 # Register your models here.
