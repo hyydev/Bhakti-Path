0e59b159ddd2d4a22ad9d32c9eb8eee154189e1b
Devesh sharma <devsharma13314@gmail.com>
2026-05-20 17:09:54 +0530

    create new venv name bvenv

--- BhaktiVerse/settings.py
+++ BhaktiVerse/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
-from decouple import config
+from decouple import config 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
